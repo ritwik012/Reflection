326fdb7fdf1f65370cf213ef234bf064dd821344
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-02 13:49:21 +0530

    [Rutwik]Added:ReflectionProgram File

---
+++ Reflection/Customer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reflection
+{
+    class Customer
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Name
+        {
+            get;
+            set;
+        }
+        public Customer(int id, string name)
+        {
+            this.Id = id;
+            this.Name = name;
+        }
+        public Customer()
+        {
+            this.Id = -1;
+            this.Name = string.Empty;
+        }
+        public void PrintId()
+        {
+            Console.WriteLine("Id is : {0}", this.Id);
+        }
+        public void PrintName()
+        {
+            Console.WriteLine("Name is : {0}", this.Name);
+        }
+    }
+}
---
+++ Reflection/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Reflection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Reflection Testing");
+            ReflectionTest.Test();
+        }
+    }
+}
